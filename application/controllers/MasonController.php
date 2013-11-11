f4c783f3b4a1db600d3a883e247316d8fb1e1cf9
B.ouwehand <b.ouwehand@synocom.nl>
2013-11-11 14:00:30 +0100

    Masonry working with Jquery

---
+++ application/controllers/MasonController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bas
+ * Date: 11/11/13
+ * Time: 10:28 AM
+ */
+ class MasonController extends Zend_Controller_Action {
+
+    public function init()
+    {
+        /* Initialize action controller here */
+
+
+
+    }
+
+    public function indexAction()
+    {
+        // action body
+
+
+
+    }
+
+
+
+
+}
