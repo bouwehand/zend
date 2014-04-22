6610ee837be7208d09d817827c314c6abd193d95
b.ouwehand <b.ouwehand@synocom.nl>
2014-04-22 13:02:21 +0200

    Starting migration and installer scripts

--- scripts/schema.mysql.sql
+++ scripts/schema.mysql.sql
@@ -6,18 +6,6 @@ CREATE TABLE guestbook (
     comment TEXT NULL,
     created DATETIME NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=innodb DEFAULT CHARSET=utf8;
  
-CREATE INDEX id ON guestbook(id);MENT, 
-	email CARCHAR(32) NOT NULL DEFAULT '    -- scripts/schema.sqlite.sql
-    --
-    -- You will need load your database schema with this SQL.
-     
-    CREATE TABLE guestbook (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        email VARCHAR(32) NOT NULL DEFAULT 'noemail@test.com',
-        comment TEXT NULL,
-        created DATETIME NOT NULL
-    );
-     
-    CREATE INDEX "id" ON "guestbook" ("id");
+CREATE INDEX id ON guestbook(id);
